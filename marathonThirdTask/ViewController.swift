c5ffe1869087bea786113f5653ba4b55b09fc79e
Mikhail German <mikhail.german@icloud.com>
2023-03-09 20:52:00 +0500

    Initial Commit

---
+++ marathonThirdTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marathonThirdTask
+//
+//  Created by Михаил Герман on 09.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
